1dd8e859df2903b372e24d728d19ab83322fbbe5
Juojo <minarro.juanmartin@gmail.com>
2024-06-06 02:58:03 -0300

    Section links fix

--- src/components/SectionButtons.jsx
+++ src/components/SectionButtons.jsx
@@ -5,8 +5,8 @@ function SectionButtons({ section }) {
   return (
     <>
         <ul className='buttons-wrapper'>
-            {section !== 'design' ? <li><a id='design' className='btn' href="http://www.behance.com/jnmrro" target="_blank" rel="noopener noreferrer">Designer</a></li> : null}
-            {section !== 'video' ? <li><a id='video' className='btn' href="https://www.youtube.com/channel/UCnm2bHBb70hkYQ814uWc-zw" target="_blank" rel="noopener noreferrer">Video Editor</a></li> : null}
+            {section !== 'design' ? <li><a id='design' className='btn' href="#designh1" >Designer</a></li> : null}
+            {section !== 'video' ? <li><a id='video' className='btn' href="#videoh1" >Video Editor</a></li> : null}
             {/* {section !== 'developer' ? <li><a id='developer' className='btn' href="http://www.github.com/juojo" target="_blank" rel="noopener noreferrer">Developer</a></li> : null} */}
         </ul>
     </>
